50054913f92e09910ed9d0608311422e91d2b52a
David Colwell <dcdigital72@gmail.com>
2018-07-17 22:13:53 -0400

    updated to fix query issues in order to get more data from SWAPI (graphql)

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -4,23 +4,24 @@ import { gqlLodash } from './utils';
 
 // The data prop, which is provided by the wrapper below contains,
 // a `loading` key while the query is in flight and posts when it is ready
-function MyComponent({data: {loading, peopleToFilms}}) {
+function MyComponent({data: {loading, allPeople}}) {
   if (loading) return (<div> Loading... </div>);
-  let people = Object.keys(peopleToFilms);
+  console.log("peopleToFilms", allPeople.allPeople);
+//   let people = Object.keys(allPeople);
   return (
     <div>
         <h1>This is a fake div...with a head, but no body!</h1>
         <dl>
-            {people.map(name => (
-            <div key={name}>
-                <dt><b>Character:</b> {name}</dt>
-                <dt><b>Hare Color:</b> {name.blinkhairColor}</dt>
-                <dt><b>iColor:</b> {name.eyeColor}</dt>
+            {allPeople.people.map(person => (
+            <div key={person.name}>
+                <dt><b>Character:</b> {person.name}</dt>
+                <dt><b>Hare Color:</b> {person.hairColor}</dt>
+                <dt><b>iColor:</b> {person.eyeColor}</dt>
                 <dd>
                 <b>films:</b><br/>
-                {peopleToFilms[name].map(film => (
+                {/* {person.film => (
                     <div> {film} </div>
-                ))}
+                ))} */}
                 </dd>
             </div>
             ))}
@@ -30,23 +31,52 @@ function MyComponent({data: {loading, peopleToFilms}}) {
 }
 
 const query = gql`
-  query {
-    peopleToFilms: allPeople @_(get: "people") {
-      people @_(
-        keyBy: "name"
-        mapValues: "filmConnection.films"
-      ) {
-        name
-        hairColor
-        eyeColor
-        filmConnection {
-          films @_(map: "title") {
-            title
-          }
+    query {
+        allPeople {
+            people {
+            name
+            hairColor
+            eyeColor
+            mass
+            filmConnection {
+                films {
+                title
+                releaseDate
+                openingCrawl
+                }
+            }
+            starshipConnection {
+                starships {
+                name
+                passengers
+                starshipClass
+                maxAtmospheringSpeed
+                }
+            }
+            }
         }
-      }
     }
-  }
 `;
+
+// const query = gql`
+//   query {
+//     peopleToFilms: allPeople @_(get: "people") {
+//       people @_(
+//         keyBy: "name"
+//         mapValues: "filmConnection.films"
+//       ) {
+//         name
+//         hairColor
+//         eyeColor
+//         filmConnection {
+//           films @_(map: "title") {
+//             title
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
+
 
 export default gqlLodash(query)(MyComponent);
